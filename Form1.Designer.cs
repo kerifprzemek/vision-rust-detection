784f9ce74e83763b2b9ac8aa19a348d5467bc20d
kerifprzemek <51423172+kerifprzemek@users.noreply.github.com>
2022-01-25 00:27:05 +0100

    25.01.2022

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace SW_T8_9_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_From_File_PB1 = new System.Windows.Forms.Button();
+            this.button_Browse_Files_PB1 = new System.Windows.Forms.Button();
+            this.textBox_Image_Path_PB1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_procent_pow_zardzewialej = new System.Windows.Forms.Label();
+            this.label_liczba_ognisk_rdzy = new System.Windows.Forms.Label();
+            this.button_analizuj_powierzchnie = new System.Windows.Forms.Button();
+            this.button_Czysc = new System.Windows.Forms.Button();
+            this.label_procent_plam = new System.Windows.Forms.Label();
+            this.numericUpDown_numer_plamy = new System.Windows.Forms.NumericUpDown();
+            this.button_pokaz_plame = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_numer_plamy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 112);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 295);
+            this.pictureBox1.TabIndex = 49;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button_From_File_PB1
+            // 
+            this.button_From_File_PB1.Location = new System.Drawing.Point(0, 34);
+            this.button_From_File_PB1.Margin = new System.Windows.Forms.Padding(4);
+            this.button_From_File_PB1.Name = "button_From_File_PB1";
+            this.button_From_File_PB1.Size = new System.Drawing.Size(64, 54);
+            this.button_From_File_PB1.TabIndex = 51;
+            this.button_From_File_PB1.Text = "Z pliku";
+            this.button_From_File_PB1.UseVisualStyleBackColor = true;
+            this.button_From_File_PB1.Click += new System.EventHandler(this.button_From_File_PB1_Click);
+            // 
+            // button_Browse_Files_PB1
+            // 
+            this.button_Browse_Files_PB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_Browse_Files_PB1.Location = new System.Drawing.Point(389, 2);
+            this.button_Browse_Files_PB1.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Browse_Files_PB1.Name = "button_Browse_Files_PB1";
+            this.button_Browse_Files_PB1.Size = new System.Drawing.Size(37, 25);
+            this.button_Browse_Files_PB1.TabIndex = 54;
+            this.button_Browse_Files_PB1.Text = "...";
+            this.button_Browse_Files_PB1.UseVisualStyleBackColor = true;
+            this.button_Browse_Files_PB1.Click += new System.EventHandler(this.button_Browse_Files_PB1_Click);
+            // 
+            // textBox_Image_Path_PB1
+            // 
+            this.textBox_Image_Path_PB1.Location = new System.Drawing.Point(59, 2);
+            this.textBox_Image_Path_PB1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Image_Path_PB1.Name = "textBox_Image_Path_PB1";
+            this.textBox_Image_Path_PB1.Size = new System.Drawing.Size(328, 22);
+            this.textBox_Image_Path_PB1.TabIndex = 53;
+            this.textBox_Image_Path_PB1.Text = "C:\\Users\\kerif\\Documents\\Codes\\Systemy Wizyjne\\SystemyWizyjne\\fig1.jpg";
+            this.textBox_Image_Path_PB1.TextChanged += new System.EventHandler(this.textBox_Image_Path_PB1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Ścieżka:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_pokaz_plame);
+            this.groupBox1.Controls.Add(this.numericUpDown_numer_plamy);
+            this.groupBox1.Controls.Add(this.label_procent_plam);
+            this.groupBox1.Controls.Add(this.label_procent_pow_zardzewialej);
+            this.groupBox1.Controls.Add(this.label_liczba_ognisk_rdzy);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(435, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(355, 405);
+            this.groupBox1.TabIndex = 65;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ZEBRANE DANE O ZARDZEWIENIU ELEMENTU";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label_procent_pow_zardzewialej
+            // 
+            this.label_procent_pow_zardzewialej.AutoSize = true;
+            this.label_procent_pow_zardzewialej.Location = new System.Drawing.Point(8, 110);
+            this.label_procent_pow_zardzewialej.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_procent_pow_zardzewialej.Name = "label_procent_pow_zardzewialej";
+            this.label_procent_pow_zardzewialej.Size = new System.Drawing.Size(297, 17);
+            this.label_procent_pow_zardzewialej.TabIndex = 78;
+            this.label_procent_pow_zardzewialej.Text = "Procentowa wartość powierzchni zardzewiałej:";
+            // 
+            // label_liczba_ognisk_rdzy
+            // 
+            this.label_liczba_ognisk_rdzy.AutoSize = true;
+            this.label_liczba_ognisk_rdzy.Location = new System.Drawing.Point(8, 51);
+            this.label_liczba_ognisk_rdzy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_liczba_ognisk_rdzy.Name = "label_liczba_ognisk_rdzy";
+            this.label_liczba_ognisk_rdzy.Size = new System.Drawing.Size(129, 17);
+            this.label_liczba_ognisk_rdzy.TabIndex = 77;
+            this.label_liczba_ognisk_rdzy.Text = "Liczba ognisk rdzy:";
+            // 
+            // button_analizuj_powierzchnie
+            // 
+            this.button_analizuj_powierzchnie.Location = new System.Drawing.Point(144, 33);
+            this.button_analizuj_powierzchnie.Margin = new System.Windows.Forms.Padding(4);
+            this.button_analizuj_powierzchnie.Name = "button_analizuj_powierzchnie";
+            this.button_analizuj_powierzchnie.Size = new System.Drawing.Size(177, 55);
+            this.button_analizuj_powierzchnie.TabIndex = 71;
+            this.button_analizuj_powierzchnie.Text = "Analizuj powierzchnę";
+            this.button_analizuj_powierzchnie.UseVisualStyleBackColor = true;
+            this.button_analizuj_powierzchnie.Click += new System.EventHandler(this.button_analizuj_powierzchne_Click);
+            // 
+            // button_Czysc
+            // 
+            this.button_Czysc.Location = new System.Drawing.Point(72, 34);
+            this.button_Czysc.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Czysc.Name = "button_Czysc";
+            this.button_Czysc.Size = new System.Drawing.Size(64, 54);
+            this.button_Czysc.TabIndex = 81;
+            this.button_Czysc.Text = "Czyść";
+            this.button_Czysc.UseVisualStyleBackColor = true;
+            this.button_Czysc.Click += new System.EventHandler(this.button_Czysc_Click);
+            // 
+            // label_procent_plam
+            // 
+            this.label_procent_plam.AutoSize = true;
+            this.label_procent_plam.Location = new System.Drawing.Point(8, 223);
+            this.label_procent_plam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_procent_plam.Name = "label_procent_plam";
+            this.label_procent_plam.Size = new System.Drawing.Size(172, 17);
+            this.label_procent_plam.TabIndex = 79;
+            this.label_procent_plam.Text = "Procentowa wartość plam:";
+            // 
+            // numericUpDown_numer_plamy
+            // 
+            this.numericUpDown_numer_plamy.Location = new System.Drawing.Point(152, 175);
+            this.numericUpDown_numer_plamy.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown_numer_plamy.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_numer_plamy.Name = "numericUpDown_numer_plamy";
+            this.numericUpDown_numer_plamy.Size = new System.Drawing.Size(80, 22);
+            this.numericUpDown_numer_plamy.TabIndex = 81;
+            this.numericUpDown_numer_plamy.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button_pokaz_plame
+            // 
+            this.button_pokaz_plame.Location = new System.Drawing.Point(11, 171);
+            this.button_pokaz_plame.Margin = new System.Windows.Forms.Padding(4);
+            this.button_pokaz_plame.Name = "button_pokaz_plame";
+            this.button_pokaz_plame.Size = new System.Drawing.Size(133, 28);
+            this.button_pokaz_plame.TabIndex = 82;
+            this.button_pokaz_plame.Text = "Pokaż plamę nr:";
+            this.button_pokaz_plame.UseVisualStyleBackColor = true;
+            this.button_pokaz_plame.Click += new System.EventHandler(this.button_pokaz_plame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(840, 451);
+            this.Controls.Add(this.button_Czysc);
+            this.Controls.Add(this.button_analizuj_powierzchnie);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_Browse_Files_PB1);
+            this.Controls.Add(this.textBox_Image_Path_PB1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_From_File_PB1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Wykrywanie plam rdzy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_numer_plamy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_From_File_PB1;
+        private System.Windows.Forms.Button button_Browse_Files_PB1;
+        private System.Windows.Forms.TextBox textBox_Image_Path_PB1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_analizuj_powierzchnie;
+        private System.Windows.Forms.Label label_liczba_ognisk_rdzy;
+        private System.Windows.Forms.Button button_Czysc;
+        private System.Windows.Forms.Label label_procent_pow_zardzewialej;
+        private System.Windows.Forms.Label label_procent_plam;
+        private System.Windows.Forms.Button button_pokaz_plame;
+        private System.Windows.Forms.NumericUpDown numericUpDown_numer_plamy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
+using System.Threading;
+
+namespace SW_T8_9_10
+{
+    public partial class Form1 : Form
+    {
+
+        private Size desired_image_size;
+        Image<Bgr, byte> image_PB1;
+        VideoCapture camera;
+
+        Queue<Point> pix_tlace = new Queue<Point>();
+        Queue<Point> pix_palace = new Queue<Point>();
+        Queue<Point> pix_nadpalone = new Queue<Point>();
+        Queue<Point> pix_wypalone = new Queue<Point>();
+
+        private MCvScalar cecha_palnosci = new MCvScalar(0xFF, 0xFF, 0xFF);
+        private MCvScalar cecha_nadpalenia = new MCvScalar(0, 0, 0);
+
+        private MCvScalar kolor_tlenia = new MCvScalar(51, 153, 255);
+        private MCvScalar kolor_palenia = new MCvScalar(0, 0, 204);
+        private MCvScalar kolor_nadpalenia = new MCvScalar(51, 204, 51);
+        private MCvScalar kolor_wypalenia = new MCvScalar(100, 100, 100);
+        private MCvScalar aktualny_kolor_wypalenia = new MCvScalar(100, 100, 100);
+
+        private int nr_pozaru = 0;
+        private float WypaloneCalosc = 0;
+        private float ProcentPowierzchni = 0;
+        private float CalaPowierzchnia = 125965;
+        private float[] ArrayPowierzchniePojedynczychPlam = new float[100];
+        private float[] TabPow = new float[100];
+        private float[] TabProcentPow = new float[100];
+
+        public Form1()
+        {
+            InitializeComponent();
+            desired_image_size = pictureBox1.Size;
+            image_PB1 = new Image<Bgr, byte>(desired_image_size);
+            try
+            {
+                camera = new VideoCapture();
+                camera.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, desired_image_size.Width);
+                camera.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, desired_image_size.Height);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void button_Browse_Files_PB1_Click(object sender, EventArgs e)
+        {
+            textBox_Image_Path_PB1.Text = get_image_path();
+        }
+
+        private string get_image_path()
+        {
+            string ret = "";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Obrazy|*.jpg;*.jpeg;*.png;*.bmp";
+            openFileDialog1.Title = "Wybierz obrazek.";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ret = openFileDialog1.FileName;
+            }
+
+            return ret;
+        }
+
+        private void button_From_File_PB1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = get_image_bitmap_from_file(textBox_Image_Path_PB1.Text, ref image_PB1);
+            Wyczysc_dane_pozaru();
+        }
+
+        private Bitmap get_image_bitmap_from_file(string path, ref Image<Bgr, byte> Data)
+        {
+            Mat temp = CvInvoke.Imread(path);
+            CvInvoke.Resize(temp, temp, desired_image_size);
+            Data = temp.ToImage<Bgr, byte>();
+            return Data.Bitmap;
+        }
+
+        private void Wyczysc_dane_pozaru()
+        {
+            nr_pozaru = 0;
+            pix_wypalone.Clear();
+        }
+
+        private void Pozar_Calosci()
+        {
+
+            byte[,,] pozar = image_PB1.Data;
+            for (int y = 1; y < desired_image_size.Height - 2; y++)
+            {
+                for (int x = 1; x < desired_image_size.Width - 2; x++)
+                {
+                    if (Sprawdz_czy_cecha_palnosci(pozar[y, x, 0], pozar[y, x, 1], pozar[y, x, 2]))
+                    {
+                        nr_pozaru = nr_pozaru + 1;
+
+                        aktualny_kolor_wypalenia.V0 = kolor_wypalenia.V0 + 8 * nr_pozaru;
+                        aktualny_kolor_wypalenia.V1 = kolor_wypalenia.V1 + 8 * nr_pozaru;
+                        aktualny_kolor_wypalenia.V2 = kolor_wypalenia.V2 + 8 * nr_pozaru;
+                        pix_tlace.Enqueue(new Point(x, y));
+
+                        Cykl_Pozaru();
+
+                        pozar = image_PB1.Data;
+
+                        ArrayPowierzchniePojedynczychPlam[nr_pozaru - 1] = pix_wypalone.Count();
+
+                    }
+                }
+            }
+            label_liczba_ognisk_rdzy.Text = "Liczba ognisk rdzy: " + nr_pozaru;
+            WypaloneCalosc = pix_wypalone.Count();
+            procent_pow_zardzewialej();
+            procent_pow_poszczegolnych_plam();
+            image_PB1.Data = pozar;
+            pictureBox1.Image = image_PB1.Bitmap;
+        }
+
+        private void Cykl_Pozaru()
+        {
+
+            do
+            {
+                Krok_Pozaru();
+            } while (pix_tlace.Count > 0);
+
+        }
+
+        private void Krok_Pozaru()
+        {
+            
+            byte[, ,] temp = image_PB1.Data;
+
+            Tlace_do_palacych(temp);
+
+            foreach (Point pix in pix_palace)
+            {
+                Tlenie_od_palacego(temp, pix);
+            }
+
+            foreach (Point pix in pix_palace)
+            {
+                Nadpalenie_palacego(temp, pix);
+            }
+
+            Wypalenie_palacego(temp);
+
+            image_PB1.Data = temp;
+            pictureBox1.Image = image_PB1.Bitmap;         
+            Application.DoEvents();
+        }
+
+        private void Tlace_do_palacych(byte[, ,] temp)
+        {
+            while (pix_tlace.Count > 0)
+            {
+                Point p = pix_tlace.Dequeue();
+                pix_palace.Enqueue(p);
+                temp[p.Y, p.X, 0] = (byte)kolor_palenia.V0;
+                temp[p.Y, p.X, 1] = (byte)kolor_palenia.V1;
+                temp[p.Y, p.X, 2] = (byte)kolor_palenia.V2;
+            }
+        }
+
+        private void Tlenie_od_palacego(byte[, ,] temp, Point pix_in)
+        {
+            if (Czy_piksel_w_zakresie(pix_in))
+            {
+                Point[] sasiedzi = Wylicz_wspolrzedne_sasiednich_pikseli(pix_in);
+                foreach (Point p in sasiedzi)
+                {
+                    if (Sprawdz_czy_cecha_palnosci(temp[p.Y, p.X, 0], temp[p.Y, p.X, 1], temp[p.Y, p.X, 2]))
+                    {
+                        pix_tlace.Enqueue(new Point(p.X, p.Y));
+                        temp[p.Y, p.X, 0] = (byte)kolor_tlenia.V0;
+                        temp[p.Y, p.X, 1] = (byte)kolor_tlenia.V1;
+                        temp[p.Y, p.X, 2] = (byte)kolor_tlenia.V2;
+                    }
+                }
+            }
+        }
+
+        private void Nadpalenie_palacego(byte[, ,] temp, Point pix_in)
+        {
+           
+            if (Czy_piksel_w_zakresie(pix_in))
+            {
+                Point[] sasiedzi = Wylicz_wspolrzedne_sasiednich_pikseli(pix_in);
+                bool nalezy_nadpalic = false;
+                foreach (Point p in sasiedzi)
+                {
+
+                    nalezy_nadpalic = Sprawdz_czy_cecha_nadpalenia(temp[p.Y, p.X, 0], temp[p.Y, p.X, 1], temp[p.Y, p.X, 2]);
+
+                    if (nalezy_nadpalic)
+                    {
+                        pix_nadpalone.Enqueue(new Point(p.X, p.Y));
+                        temp[p.Y, p.X, 0] = (byte)kolor_nadpalenia.V0;
+                        temp[p.Y, p.X, 1] = (byte)kolor_nadpalenia.V1;
+                        temp[p.Y, p.X, 2] = (byte)kolor_nadpalenia.V2;
+                    }
+                }
+            }
+        }
+
+        private void Wypalenie_palacego(byte[, ,] temp)
+        {
+            while (pix_palace.Count > 0)
+            {
+                Point p = pix_palace.Dequeue();
+                pix_wypalone.Enqueue(p);
+                temp[p.Y, p.X, 0] = (byte)(aktualny_kolor_wypalenia.V0);
+                temp[p.Y, p.X, 1] = (byte)(aktualny_kolor_wypalenia.V1);
+                temp[p.Y, p.X, 2] = (byte)(aktualny_kolor_wypalenia.V2);
+            }
+        }
+
+        private Point[] Wylicz_wspolrzedne_sasiednich_pikseli(Point pix_in)
+        {
+            List<Point> sasiedzi = new List<Point>();
+            sasiedzi.Add(new Point(pix_in.X - 1, pix_in.Y));
+            sasiedzi.Add(new Point(pix_in.X + 1, pix_in.Y));
+            sasiedzi.Add(new Point(pix_in.X, pix_in.Y - 1));
+            sasiedzi.Add(new Point(pix_in.X, pix_in.Y + 1));
+
+            return sasiedzi.ToArray();
+        }
+
+        private bool Czy_piksel_w_zakresie(Point pix_in)
+        {
+            int max_W, max_H;
+            max_W = desired_image_size.Width - 1;
+            max_H = desired_image_size.Height - 1;
+            if (pix_in.X > 0 && pix_in.X < max_W && pix_in.Y > 0 && pix_in.Y < max_H)
+                return true;
+            else
+                return false;
+        }
+
+        private bool Sprawdz_czy_cecha_palnosci(byte B, byte G, byte R)
+        {
+            if (B == 0x0 && G == 0x0 && R == 0x0)
+                return true;
+            else
+                return false;
+        }
+
+        private bool Sprawdz_czy_cecha_nadpalenia(byte B, byte G, byte R)
+        {
+            if (B == 0xff && G == 0xff && R == 0xff)
+                return true;
+            else
+                return false;
+        }
+
+        private bool Sprawdz_czy_jakiekolwiek_nadpalenie(byte B, byte G, byte R)
+        {
+            if (B == 0x0 && G == 0x0 && R == 0x0)
+                return false;
+            else if (B == 0xff && G == 0xff && R == 0xff)
+                return true;
+            else if (B == kolor_tlenia.V0 && G == kolor_tlenia.V1 && R == kolor_tlenia.V2)
+                return false;
+            else if (B == kolor_nadpalenia.V0 && G == kolor_nadpalenia.V1 && R == kolor_nadpalenia.V2)
+                return false;
+            else if (B == kolor_palenia.V0 && G == kolor_palenia.V1 && R == kolor_palenia.V2)
+                return false;
+            else if (B == aktualny_kolor_wypalenia.V0 && G == aktualny_kolor_wypalenia.V1 && R == aktualny_kolor_wypalenia.V2)
+                return false;
+            else
+                return true;
+        }
+
+        private void button_analizuj_powierzchne_Click(object sender, EventArgs e)
+        {
+            Wyczysc_dane_pozaru();
+            Application.DoEvents();
+            Pozar_Calosci();
+        }
+
+        private void procent_pow_zardzewialej()
+        {
+
+            ProcentPowierzchni = WypaloneCalosc / CalaPowierzchnia * 100;
+            label_procent_pow_zardzewialej.Text = "Procentowa wartość powierzchni zardzewiałej: \n" + ProcentPowierzchni + " %";
+            
+        }
+
+
+        private void procent_pow_poszczegolnych_plam()
+        {
+            
+            for (int i = 0; i< nr_pozaru; i++)
+            {
+                if (i == 0)
+                {
+                    TabPow[i] = ArrayPowierzchniePojedynczychPlam[i];
+                }
+                else
+                {
+                    TabPow[i] = ArrayPowierzchniePojedynczychPlam[i] - ArrayPowierzchniePojedynczychPlam[i-1];
+                }
+                
+                TabProcentPow[i] = TabPow[i] / CalaPowierzchnia * 100;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                CvInvoke.Circle(image_PB1, e.Location, 2, new MCvScalar(255, 255, 255), -1);
+                pictureBox1.Image = image_PB1.Bitmap;
+            }
+        }
+
+        private void button_Czysc_Click(object sender, EventArgs e)
+        {
+            image_PB1.SetZero();
+            pictureBox1.Image = image_PB1.Bitmap;
+        }
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_Image_Path_PB1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown_Numer_obiektu_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_pokaz_plame_Click(object sender, EventArgs e)
+        {
+            label_procent_plam.Text = "Procentowa wartość plamy " + numericUpDown_numer_plamy.Value + ": " + TabProcentPow[(int)numericUpDown_numer_plamy.Value-1] + " %";
+        }
+    }
+}
